8ef5515a8838d38393903fc3f45692da2849d040
Shaunak Kashyap <ycombinator@gmail.com>
2024-06-10 16:56:28 -0700

    Rename module kardianos/service to elastic/go-service

--- example/stopPause/main.go
+++ example/stopPause/main.go
@@ -10,7 +10,7 @@ import (
 	"os"
 	"time"
 
-	"github.com/kardianos/service"
+	"github.com/elastic/go-service"
 )
 
 var logger service.Logger
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module github.com/kardianos/service
+module github.com/elastic/go-service
 
 go 1.12
 
